3447f698128897c139bcee6b6bf99a3382637478
Bamorph <142395807+Bamorph@users.noreply.github.com>
2023-10-09 11:24:18 +1100

    update comments

--- PTSpam.sh
+++ PTSpam.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-# Credit to John Hammond for this idea
+#   __                                     __    
+#  |  |--.---.-.--------.-----.----.-----.|  |--.
+#  |  _  |  _  |        |  _  |   _|  _  ||     |
+#  |_____|___._|__|__|__|_____|__| |   __||__|__|
+#                                |__|          
+#  
+#  Author: Bamorph
+#  Year: 2023
+#  Credit: John Hammond
+#  
+#  Notes: John hammond used the base version of this code in king of the hill agains Optional and others. 
+#  The original code was to use the tty command to determin your own tty session and then 
+#  'cat /dev/urandom > /dev/pts/5' into someone elses pts session. the result is the other
+#  persons screen being completly spammed by random data.
+
 
 # Get the current PTS (your PTS)
 current_pts=$(tty | cut -d '/' -f 4)
